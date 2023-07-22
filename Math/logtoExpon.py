71018916790ff012c03acad566d0ace091a93638
Nathan A <21artern@gmail.com>
2023-07-22 12:58:52 -0500

    typo

--- Math/logtoExpon.py
+++ Math/logtoExpon.py
@@ -7,13 +7,13 @@ while True:
     choice = int(choice)
     if 0 < choice < 4:
         if choice == 2:
-            print("LogA(b)=x")
+            print("A^x = b")
             a = input("A >")
             b = input("b >")
             x = input("x >")
             print("log" + a + "(" + b + ")" + "=" + x)
         elif choice == 1:
-            print("A^x = b")
+            print("LogA(b)=x")
             a = input("A >")
             b = input("x >")
             x = input("b >")
